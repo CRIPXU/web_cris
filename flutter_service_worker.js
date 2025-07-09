72d33d5cfb818ebb4423551831bd05530206e01b
FlutterDevMaster <cristofer669galo@gmail.com>
2025-07-09 17:27:22 -0400

    update image

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -134,7 +134,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "641b3c68fe0c91cae39328b0ef14f513
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "e9af3c7813db0f6a87f5b40ce6e32e0e",
+"flutter_bootstrap.js": "94b6e636474aadd98dce3aac908cebe4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
